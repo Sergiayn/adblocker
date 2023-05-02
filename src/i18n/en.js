1d309fa0db17d3a2d293298404c5a37bf6d1e2be
Sergey <SergeySm128@gmail.com>
2023-05-02 23:19:39 +0300

    init vue

---
+++ src/i18n/en.js
@@ -0,0 +1,12 @@
+const translate = {
+    common: {},
+    header: {},
+    footer: {},
+    menu: {
+        contact: "Contact",
+        home: "Home",
+        privacy: "Privacy policy",
+        terms_of_use: "Terms of use"
+    },
+}
+export default translate
---
+++ src/i18n/es.js
@@ -0,0 +1,12 @@
+const translate = {
+    common: {},
+    header: {},
+    footer: {},
+    menu: {
+        contact: "Contacto",
+        home: "Hogar",
+        privacy: "Política de privacidad",
+        terms_of_use: "Condiciones de uso"
+    },
+}
+export default translate
